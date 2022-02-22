6886b8902aa9da903d45c78ea14c2b4dd3a99c7b
XBGe <gexinbin@gmail.com>
2022-02-22 11:20:37 +1100

    done to push

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -15,10 +15,7 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            This is help  you to tracking you jobs processing when you are applying many jobs，Recording and Tracking the job processing
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
